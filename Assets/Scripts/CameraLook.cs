a0aa784c4bd97b8a3824f3090fb633d7ce8b16fa
Jonathan Kaufmann <jonathan@wirelessbites.com>
2014-10-27 14:36:37 -0500

    Initial commit

---
+++ Assets/Scripts/CameraLook.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLook : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+		var cameraCenter = camera.ScreenToWorldPoint(new Vector3(Screen.width / 2f, Screen.height / 2f, camera.nearClipPlane));
+		if (Physics.Raycast(cameraCenter, this.transform.forward, out hit, 1000))
+		{
+			var obj = hit.transform.gameObject;
+			hit.transform.Rotate(1.0f, 1.0f, 1.0f);
+		}
+	}
+}
