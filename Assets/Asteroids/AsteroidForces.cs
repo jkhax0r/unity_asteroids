68c9cfcdd61cfe51b939c5ad9420dc605a094f52
nicholas <nicholas.kaufmann@gmail.com>
2014-11-05 13:57:26 -0600

    Added forces to asteroid, added tags to border and asteroid.

--- Assets/Asteroids/AsteroidForces.cs
+++ Assets/Asteroids/AsteroidForces.cs
@@ -3,13 +3,20 @@ using System.Collections;
 
 public class AsteroidForces : MonoBehaviour {
 
+	Vector3 randDirection;
+	float SCALE = 10.0f;
+
 	// Use this for initialization
 	void Start () {
-	
+		float randX = Random.Range (-10.0f, 10.0f);
+		float randY = Random.Range (-10.0f, 10.0f);
+		float randZ = Random.Range (-10.0f, 10.0f);
+		randDirection = new Vector3 (randX, randY, randZ);
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		this.rigidbody.AddForce (randDirection * 1.0f);
 	
 	}
 }
